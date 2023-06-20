5d2ac9467e2e773419532ddefe81c186eb1b2022
José Iriel de Freitas Martins <jose.iriel.martins@gmail.com>
2023-06-19 21:32:28 -0300

    Soluciona o exercício "Bank Account"

---
+++ javascript/bank-account/bank-account.js
@@ -0,0 +1,43 @@
+// Naïve solution
+
+export class BankAccount {
+  #isOpen = false;
+  #balance = 0;
+
+  constructor() {}
+
+  open() {
+    if (this.#isOpen) throw new ValueError("account is already open");
+    this.#isOpen = true;
+  }
+
+  close() {
+    if (!this.#isOpen) throw new ValueError("account is not open");
+    this.#balance = 0;
+    this.#isOpen = false;
+  }
+
+  deposit(amount) {
+    if (!this.#isOpen) throw new ValueError("account is not open");
+    if (amount <= 0) throw new ValueError("`amount` must be positive");
+    this.#balance += amount;
+  }
+
+  withdraw(amount) {
+    if (!this.#isOpen) throw new ValueError("account is not open");
+    if (amount <= 0 || amount > this.balance)
+      throw new ValueError("`amount` must be positive");
+    this.#balance -= amount;
+  }
+
+  get balance() {
+    if (!this.#isOpen) throw new ValueError("account is not open");
+    return this.#balance;
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super("Bank account error");
+  }
+}
